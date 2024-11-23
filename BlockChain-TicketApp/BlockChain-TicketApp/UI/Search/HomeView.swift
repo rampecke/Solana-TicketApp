c77b7a52a75027989f483cea5ea196ac4a06319f
Ramona_Eckert <ge36moc@mytum.de>
2024-11-23 21:49:56 +0100

    Use Model in HomeView

--- BlockChain-TicketApp/BlockChain-TicketApp/UI/Search/HomeView.swift
+++ BlockChain-TicketApp/BlockChain-TicketApp/UI/Search/HomeView.swift
@@ -11,13 +11,30 @@ struct HomeView: View {
     @State var searchText: String = ""
     @State private var isEditing = false // State to track if the user is editing
     
+    func selectSortOption(eventType: EventCategoryType) -> SortOptions {
+        switch eventType {
+        case .Music:
+            return SortOptions.Music
+        case .Sports:
+            return SortOptions.Sports
+        case .ArtsTheatre:
+            return SortOptions.ArtsTheatre
+        case .Conference:
+            return SortOptions.Conference
+        case .Fair:
+            return SortOptions.Fair
+        case .Other:
+            return SortOptions.Other
+        }
+    }
+    
     var body: some View {
         ScrollView{
             VStack(alignment: .leading) {
-                EventSection(sectionName: "Popular Events")
-                EventSection(sectionName: "Nearby Event")
+                EventSection(sectionName: "Popular Events", sortOption: SortOptions.Popular)
+                EventSection(sectionName: "Nearby Event", sortOption: SortOptions.CloseBy)
                 ForEach(EventCategoryType.allCases, id: \.self) { category in
-                    EventSection(sectionName: category.rawValue)
+                    EventSection(sectionName: category.rawValue, sortOption: selectSortOption(eventType: category))
                 }
             }
         }
