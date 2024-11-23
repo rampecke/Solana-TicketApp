c55e21363da978218b4bc40248ae8f48bff383cf
Ramona_Eckert <ge36moc@mytum.de>
2024-11-23 03:23:12 +0100

    Added Swift Project

---
+++ BlockChain-TicketApp/BlockChain-TicketApp/BlockChain_TicketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockChain_TicketAppApp.swift
+//  BlockChain-TicketApp
+//
+//  Created by Ramona Eckert on 23.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BlockChain_TicketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
