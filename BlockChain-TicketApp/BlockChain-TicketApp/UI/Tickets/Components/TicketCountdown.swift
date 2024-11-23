6656510aa26d0273a5ee9eb478e28e1f3a3d24ba
Ramona_Eckert <ge36moc@mytum.de>
2024-11-23 20:55:36 +0100

    Added a TicketCard with a countdown

---
+++ BlockChain-TicketApp/BlockChain-TicketApp/UI/Tickets/Components/TicketCountdown.swift
@@ -0,0 +1,38 @@
+//
+//  TicketCountdown.swift
+//  BlockChain-TicketApp
+//
+//  Created by Ramona Eckert on 23.11.24.
+//
+
+import SwiftUI
+
+struct TicketCountdown: View {
+    @Bindable var ticket: Ticket
+    @Binding var remaningTime: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(ticket.organizationEvent.eventName)
+                .font(.headline)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            HStack{
+                Text(remaningTime)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }.frame(maxWidth: .infinity, alignment: .leading)
+        }.padding(10)
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(.background)
+        ).frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+#Preview {
+    @State var remaning: String = "10:08:09:11"
+    return TicketCountdown(ticket: MockModel().getExampleTicket(), remaningTime: $remaning)
+}
---
+++ BlockChain-TicketApp/BlockChain-TicketApp/UI/Tickets/Components/TicketCountdownElement.swift
@@ -0,0 +1,57 @@
+//
+//  TicketCountdownElement.swift
+//  BlockChain-TicketApp
+//
+//  Created by Ramona Eckert on 23.11.24.
+//
+
+import SwiftUI
+
+struct TicketCountdownElement: View {
+    @Bindable var ticket: Ticket
+    
+    //Countdown
+    @State private var remainingTime: String = ""
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    private func updateRemainingTime() {
+        let now = Date()
+        let targetDate = ticket.organizationEvent.startTime
+        let components = Calendar.current.dateComponents(
+            [.day, .hour, .minute, .second],
+            from: now,
+            to: targetDate
+        )
+        
+        if let days = components.day, let hours = components.hour, let minutes = components.minute, let seconds = components.second {
+            remainingTime = String(format: "%02d:%02d:%02d:%02d", days, hours, minutes, seconds)
+        } else {
+            remainingTime = "00:00:00:00" // Countdown finished or invalid
+        }
+    }
+
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Image("ExamplePicture") // TODO: Replace with my image name
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: .infinity, maxHeight: 250, alignment: .topLeading)
+                .clipped()
+                .cornerRadius(15)
+            
+            TicketCountdown(ticket: ticket, remaningTime: $remainingTime)
+                .padding(.bottom, 10)
+                .padding(.horizontal, 20)
+        }.onAppear {
+            updateRemainingTime()
+        }.onReceive(timer) { _ in
+            updateRemainingTime()
+        }
+    }
+}
+
+#Preview {
+    let model = MockModel()
+    return TicketCountdownElement(ticket: model.getExampleTicket())
+}
