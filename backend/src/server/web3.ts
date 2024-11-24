ebc4007e61360fb371c5de74b3172b4eaa9a8ce5
Simon Osterlehner <simon@osterlehner.de>
2024-11-24 06:53:09 +0100

    Adjust type errors in web3

--- backend/src/server/web3.ts
+++ backend/src/server/web3.ts
@@ -1,26 +1,35 @@
-import { clusterApiUrl, Keypair, PublicKey } from '@solana/web3.js';
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { createSignerFromKeypair, signerIdentity } from '@metaplex-foundation/umi';
-import { TokenStandard, mplTokenMetadata, transferV1 } from '@metaplex-foundation/mpl-token-metadata';
-import * as dotenv from 'dotenv';
+import { clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import {
+  TokenStandard,
+  mplTokenMetadata,
+  transferV1,
+} from "@metaplex-foundation/mpl-token-metadata";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const WALLET_SECRET_KEY = process.env.WALLET_SECRET_KEY;
 
-const umi = createUmi(clusterApiUrl('devnet')).use(mplTokenMetadata())
+const umi = createUmi(clusterApiUrl("devnet")).use(mplTokenMetadata());
 
-const keypair1 = Keypair.fromSecretKey(new Uint8Array(JSON.parse(WALLET_SECRET_KEY ?? '')));
+const keypair1 = Keypair.fromSecretKey(
+  new Uint8Array(JSON.parse(WALLET_SECRET_KEY ?? ""))
+);
 
-// Usually Keypairs are saved as Uint8Array, so you  
-// need to transform it into a usable keypair.  
+// Usually Keypairs are saved as Uint8Array, so you
+// need to transform it into a usable keypair.
 let keypair = umi.eddsa.createKeypairFromSecretKey(keypair1.secretKey);
 
-// Before Umi can use this Keypair you need to generate 
-// a Signer type with it.  
+// Before Umi can use this Keypair you need to generate
+// a Signer type with it.
 const signer = createSignerFromKeypair(umi, keypair);
 
 // Tell Umi to use the new signer.
-umi.use(signerIdentity(signer))
+umi.use(signerIdentity(signer));
 
 // Send an NFT to the provided address
 export const sendNFT = async (nftAddress: string, recipientAddress: string) => {
@@ -28,14 +37,12 @@ export const sendNFT = async (nftAddress: string, recipientAddress: string) => {
   const recipientPublicKey = new PublicKey(recipientAddress);
 
   const transaction = await transferV1(umi, {
-      mint: nftPublicKey,
-      authority: signer,
-      tokenOwner: signer.publicKey,
-      destinationOwner: recipientPublicKey,
-      tokenStandard: TokenStandard.NonFungible,
-    }).sendAndConfirm(umi)
+    mint: nftPublicKey as any,
+    authority: signer,
+    tokenOwner: signer.publicKey,
+    destinationOwner: recipientPublicKey as any,
+    tokenStandard: TokenStandard.NonFungible,
+  }).sendAndConfirm(umi);
   console.log(transaction);
-  console.log(
-    `NFT sent to ${recipientAddress}.`,
-  );
+  console.log(`NFT sent to ${recipientAddress}.`);
 };
